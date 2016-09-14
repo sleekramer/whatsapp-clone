79a4605505b00961ad0eb5ae8fd10f6d6acc8440
Steven Lee-Kramer <sleekramer@gmail.com>
2016-09-14 18:11:36 -0400

    add ability to create and destroy chats

--- client/scripts/controllers/chats.controller.js
+++ client/scripts/controllers/chats.controller.js
@@ -12,9 +12,14 @@ export default class ChatsCtrl extends Controller {
       }
     });
   }
+  showNewChatModal() {
+    this.NewChat.showModal();
+  }
 
   remove(chat) {
-    this.data.remove(chat);
+    this.callMethod('removeChat', chat._id);
   }
 
 }
+
+ChatsCtrl.$inject = ['NewChat'];
--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -7,17 +7,21 @@ import 'angular-ui-router';
 import 'ionic-scripts';
 import Angular from 'angular';
 import Loader from 'angular-ecmascript/module-loader';
-import { Meteor } from 'meteor/meteor'
+import { Meteor } from 'meteor/meteor';
 
 // Modules
 import ChatsCtrl from '../controllers/chats.controller';
 import ChatCtrl from '../controllers/chat.controller';
 import ConfirmationCtrl from '../controllers/confirmation.controller'
 import LoginCtrl from '../controllers/login.controller';
+import NewChatCtrl from '../controllers/new-chat.controller';
 import ProfileCtrl from '../controllers/profile.controller';
 import SettingsCtrl from '../controllers/settings.controller';
 import InputDirective from '../directives/input.directive';
 import CalendarFilter from '../filters/calendar.filter';
+import ChatNameFilter from '../filters/chat-name.filter';
+import ChatPictureFilter from '../filters/chat-picture.filter';
+import NewChatService from '../services/new-chat.service';
 import Routes from '../routes';
 
 const App = 'Whatsapp';
@@ -34,10 +38,14 @@ new Loader(App)
   .load(ChatCtrl)
   .load(ConfirmationCtrl)
   .load(LoginCtrl)
+  .load(NewChatCtrl)
   .load(ProfileCtrl)
   .load(SettingsCtrl)
   .load(InputDirective)
   .load(CalendarFilter)
+  .load(ChatNameFilter)
+  .load(ChatPictureFilter)
+  .load(NewChatService)
   .load(Routes);
 // startup
 if (Meteor.isCordova) {
